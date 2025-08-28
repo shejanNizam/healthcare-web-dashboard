7ce978bcaf4f1e2b09de20adcaddc5fafa28babe
nizam <nizam.shejan11@gmail.com>
2025-08-28 15:02:49 +0600

    fixed build issue

--- src/pages/Main/StuffSolutions/TabbedViewStaff.jsx
+++ src/pages/Main/StuffSolutions/TabbedViewStaff.jsx
@@ -1,25 +1,14 @@
 import { Tabs } from "antd";
-import { useNavigate } from "react-router-dom";
 import StaffingSolutions from "./StaffingSolutions";
 
 const { TabPane } = Tabs;
 
 export default function TabbedViewStaff() {
-  // const navigate = useNavigate();
-
   return (
     <>
-      {/* <div className="flex justify-end mr-4">
-        <button
-          onClick={() => navigate(`/add-staff`)}
-          className="text-white border bg-primary rounded-full px-6 py-2"
-        >
-          +Create
-        </button>
-      </div> */}
       <Tabs defaultActiveKey="1" centered>
         <TabPane
-          tab={<span className="font-bold text-xl">Stuffing Solutions</span>}
+          tab={<span className="font-bold text-xl">Staffing Solutions</span>}
           key="1"
         >
           <StaffingSolutions type="statting_solutions" />
